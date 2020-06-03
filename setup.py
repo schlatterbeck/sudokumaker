c18142a0075b04938a4c51df9b0b7f1ddf451aee
Ralf Schlatterbeck <rsc@runtux.com>
2020-06-03 16:41:07 +0200

    Fix License
    License was always GPL, now clarify that we mean 2 or later and
    add license file.

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-# Copyright (C) 2008-20 Dr. Ralf Schlatterbeck Open Source Consulting.
+# Copyright (C) 2008-2020 Dr. Ralf Schlatterbeck Open Source Consulting.
 # Reichergasse 131, A-3411 Weidling.
 # Web: http://www.runtux.com Email: office@runtux.com
-# All rights reserved
 # ****************************************************************************
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-# 02110-1301 USA
+# 02110-1301, USA.
 # ****************************************************************************
 
 from warnings       import filterwarnings
--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,23 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+# Copyright (C) 2005-2020 Dr. Ralf Schlatterbeck Open Source Consulting.
+# Reichergasse 131, A-3411 Weidling.
+# Web: http://www.runtux.com Email: office@runtux.com
+# ****************************************************************************
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+# ****************************************************************************
 
 from   __future__ import print_function
 import sys
