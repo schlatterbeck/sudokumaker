62f64014b35770010041b9092ae61fc8ed597f88
Ralf Schlatterbeck <rsc@runtux.com>
2020-11-12 11:36:49 +0100

    Add -i option to include digits

--- sumsudoku.py
+++ sumsudoku.py
@@ -2,7 +2,7 @@
 
 from argparse import ArgumentParser
 
-def tryall (n, digitsum, mind = 1, exclude = ()) :
+def tryall (n, digitsum, exclude = (), include = (), mind = 1) :
     """ Yield all possible combinations of n digits that sum to digitsum
     >>> list (tryall (5, 15))
     [[1, 2, 3, 4, 5]]
@@ -14,15 +14,23 @@ def tryall (n, digitsum, mind = 1, exclude = ()) :
     [[2, 7, 9], [4, 5, 9], [4, 6, 8], [5, 6, 7]]
     >>> list (tryall (4, 21, exclude = (2, 4)))
     [[1, 3, 8, 9], [1, 5, 6, 9], [1, 5, 7, 8], [3, 5, 6, 7]]
+    >>> list (tryall (4, 21, exclude = (1, 6, 7, 8), include = (9, )))
+    [[3, 4, 5, 9]]
+    >>> list (tryall (3, 23))
+    [[6, 8, 9]]
+    >>> list (tryall (3, 23, include = (6, )))
+    [[6, 8, 9]]
     """
+    include = set (include)
     for k in range (mind, 10) :
         if k in exclude :
             continue
         if (n == 1 and k == digitsum) :
             yield [k]
         else :
-            for l in tryall (n - 1, digitsum - k, k + 1, exclude = exclude) :
-                yield [k] + l
+            for l in tryall (n - 1, digitsum - k, exclude, include, k + 1) :
+                if mind > 1 or include < set ([k] + l) :
+                    yield [k] + l
 
 def main () :
     cmd = ArgumentParser ()
@@ -43,8 +51,15 @@ def main () :
         , action  = 'append'
         , default = []
         )
+    cmd.add_argument \
+        ( '-i', '--include'
+        , help    = 'Include only solutions with these numbers'
+        , type    = int
+        , action  = 'append'
+        , default = []
+        )
     args = cmd.parse_args ()
-    print (list (tryall (args.ndigits, args.digitsum, exclude = args.exclude)))
+    print (list (tryall (args.ndigits, args.digitsum, args.exclude, args.include)))
 # end def main
 
 if __name__ == '__main__' :
